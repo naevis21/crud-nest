379e601fdcc80cc1e2e2bcae598fb6bcb23d1ee9
Malu <malucifer@academico.ufs.br>
2025-01-31 10:00:42 -0300

    importando e configuramdo o TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
+import { Task } from './tasks/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TasksController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Task],
+      synchronize: true, // Cuidado: use apenas em desenvolvimento!
+    }),
+    TypeOrmModule.forFeature([Task]),
+  ],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class AppModule {}
